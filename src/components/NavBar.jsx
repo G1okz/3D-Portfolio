61abd6c671ebf8759948976a9905c93a73353f4b
G1okz <miguelreynac1@gmail.com>
2024-05-01 01:55:39 +0200

    Refactor routing in App.jsx and update NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,6 +9,7 @@ const NavBar = () => {
       <nav className="flex text-lg gap-7 font-medium">
         <NavLink to="/about" className={({ isActive }) => isActive? 'text-blue-500' : 'text-black'}>About</NavLink>
         <NavLink to="/projects" className={({ isActive }) => isActive? 'text-blue-500' : 'text-black'}>Projects</NavLink>
+      </nav>
     </header>
   )
 }
